d203ee7934e4e0c0eb153bfdd806646404682c86
Jonathan Perret <jonathan@ut7.fr>
2016-01-27 17:09:22 +0100

    Make Jorel tasks visible in `mix help`
    `mix help` used to not show any of these tasks.
    
    The @shortdoc annotations were copied from the descriptions in
    `README.md`.

--- lib/mix/tasks/jorel.deb.ex
+++ lib/mix/tasks/jorel.deb.ex
@@ -1,6 +1,8 @@
 defmodule Mix.Tasks.Jorel.Deb do
   use Mix.Task
 
+  @shortdoc "Create a Debian package with your release"
+
   def run(_) do
     {:ok, _} = Application.ensure_all_started(:jorel)
     :jorel.run([{:config, 'jorel.config'}, {:output_dir, './_jorel'}], [:deb])
