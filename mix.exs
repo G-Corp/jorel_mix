fccd14812daa6ef073acbd6299ea190826655384
glejeune <gregoire.lejeune@free.fr>
2016-01-29 12:50:40 +0100

    Bump to 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule JorelMix.Mixfile do
 
   def project do
     [app: :jorel_mix,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      description: description,
      package: package,
